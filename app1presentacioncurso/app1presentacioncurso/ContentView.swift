30cda8be7b45bcc3cc2feff7697973f84af1fa6d
Rubén <ruben.sr.apple@gmail.com>
2024-02-26 10:57:16 +0100

    Clase 1

---
+++ app1presentacioncurso/app1presentacioncurso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app1presentacioncurso
+//
+//  Created by Ruben on 26/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .font(.largeTitle)
+            .bold()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app1presentacioncurso/app1presentacioncurso/app1presentacioncursoApp.swift
@@ -0,0 +1,17 @@
+//
+//  app1presentacioncursoApp.swift
+//  app1presentacioncurso
+//
+//  Created by Ruben on 26/2/24.
+//
+
+import SwiftUI
+
+@main
+struct app1presentacioncursoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
