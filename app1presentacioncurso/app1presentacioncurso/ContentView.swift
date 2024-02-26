e0b86e7b951f04cc3c7a63ece24b771a1399b079
Rubén <ruben.sr.apple@gmail.com>
2024-02-26 11:20:54 +0100

    Clase 2

--- app1presentacioncurso/app1presentacioncurso/ContentView.swift
+++ app1presentacioncurso/app1presentacioncurso/ContentView.swift
@@ -9,10 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .font(.largeTitle)
-            .bold()
+        ZStack {
+            VStack(alignment: .leading, spacing: 8) {
+                
+                Text("Siguiendo")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Text("Canales recomendados")
+                    .foregroundStyle(.gray)
+                
+                HStack {
+                    Rectangle()
+                        .foregroundColor(.blue)
+                        .frame(width: 118, height: 68)
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Circle()
+                                .frame(width: 18,height: 18)
+                                .foregroundColor(.green)
+                            Text("username")
+                                .font(.headline)
+                        }
+                        Text("aprendiendo swiftui")
+                            .foregroundStyle(.gray)
+                        Text("💪🏻")
+                    }
+                }
+            }
+            
+            Text("SUPERPONIENDO TEXTO CON ZSTACK")
+                .font(.largeTitle)
+                .bold()
+                .foregroundStyle(.red)
+                .underline()
+                .rotationEffect(.degrees(-20))
+        }
+        
     }
 }
 
