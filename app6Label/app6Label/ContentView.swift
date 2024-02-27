24790c9c89d34c6a8ba697fba6775f65a92baf37
Rubén <ruben.sr.apple@gmail.com>
2024-02-27 10:31:34 +0100

    Clase 6 Label

---
+++ app6Label/app6Label/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app6Label
+//
+//  Created by Ruben on 27/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app6Label/app6Label/app6LabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  app6LabelApp.swift
+//  app6Label
+//
+//  Created by Ruben on 27/2/24.
+//
+
+import SwiftUI
+
+@main
+struct app6LabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
