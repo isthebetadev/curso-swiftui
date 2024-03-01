1d655dff90c380bffdddcf0e88562159ea0408b9
Rubén <ruben.sr.apple@gmail.com>
2024-03-01 11:17:56 +0100

    Clase 13 ProgressView tipo Loading y Bar

---
+++ app13ProgressView/app13ProgressView/app13ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  app13ProgressViewApp.swift
+//  app13ProgressView
+//
+//  Created by Ruben on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct app13ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
