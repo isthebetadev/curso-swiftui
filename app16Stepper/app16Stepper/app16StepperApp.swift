dbbab2921fa2fbbebbdd85c27019181840a42c62
Rubén <ruben.sr.apple@gmail.com>
2024-03-07 09:48:35 +0100

    Clase 16 - Stepper

---
+++ app16Stepper/app16Stepper/app16StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  app16StepperApp.swift
+//  app16Stepper
+//
+//  Created by Ruben on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct app16StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
