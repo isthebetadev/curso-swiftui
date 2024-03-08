c0c10bead4f9c1b2eb3928d4170cdc310e66924a
Rubén <ruben.sr.apple@gmail.com>
2024-03-08 19:09:44 +0100

    Clase 23 FullscreenCoverSheet

---
+++ app22FullscreenCover&Sheet/app22FullscreenCover&Sheet/app22FullscreenCover_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  app22FullscreenCover_SheetApp.swift
+//  app22FullscreenCover&Sheet
+//
+//  Created by Ruben on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct app22FullscreenCover_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
