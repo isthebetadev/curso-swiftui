199ccaad511bf3c1689cada9cfb4d040c5741108
Rubén <ruben.sr.apple@gmail.com>
2024-03-20 11:06:55 +0100

    Clase 38 - PreferenceKey

---
+++ app38PrefenceKey/app38PrefenceKey/app38PrefenceKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  app38PrefenceKeyApp.swift
+//  app38PrefenceKey
+//
+//  Created by Ruben on 20/3/24.
+//
+
+import SwiftUI
+
+@main
+struct app38PrefenceKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
